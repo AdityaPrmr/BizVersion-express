99ef8ef50110ad96399f330c6dc0f307775b90db
AdityaPrmr <parmar2100parmar@gamil.com>
2025-02-12 18:04:08 +0530

    Updated error dis

--- config/db.js
+++ config/db.js
@@ -4,11 +4,12 @@ const connectDB = async()=>{
 
     try
     {
-        await mongoose.connect(process.env.MONGO_URI, {
+        mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true
-        });
-        console.log('MongoDB Connected');
+        })
+        .then(() => console.log("Connected to MongoDB"))
+        .catch(err => console.error("MongoDB connection error:", err));
     }
     catch(error)
     {
