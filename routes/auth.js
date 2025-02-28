636ab9be972c4a73d0c48fb9e1ff780beb9f7bb5
AdityaPrmr <parmar2100parmar@gamil.com>
2025-02-28 23:14:10 +0530

    Updated project with latest changes

--- routes/auth.js
+++ routes/auth.js
@@ -44,4 +44,53 @@ router.post("/login",async(req,res)=>{
     }
 });
 
+
+router.post("/detail",async(req,res)=>{
+    try
+    {
+        let { _id } = req.body;
+        // Ensure _id is a valid ObjectId
+        if (!_id || typeof _id !== "string") {
+            return res.status(400).json({ error: "Invalid Employee ID format" });
+        }
+
+        _id = _id.replace(/"/g, ""); // Remove extra quotes
+
+        if (!mongoose.Types.ObjectId.isValid(_id)) {
+            return res.status(400).json({ error: "Invalid Employee ID" });
+        }
+        const user = await User.findOne({ _id });
+        if(!user)
+        {
+            const b = await Bussiness.findOne({_id });
+            if(!b)
+            {
+                const e = await Employee.findOne({_id });
+                if(!e)
+                {
+                    res.status(404).json({message:"User not found"});
+                    console.log(e);
+                }
+                else
+                {
+                    res.status(200).json(e);
+                }
+            }
+            else
+            {
+                res.status(200).json(b);
+            }
+
+        }
+        else
+        {
+            res.status(200).json(user);
+        }
+    }
+    catch(error)
+    {
+        res.status(500).json({error: error.message});
+    }
+});
+
 module.exports = router;
